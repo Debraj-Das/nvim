112b16d53e0b5140b74ae4dab833b844478ab6f3
Debraj Das <debrajdas.gayatri@gmail.com>
2025-01-13 19:51:35 +0530

    autocompleted key binding add for {

--- lua/debraj/keybinding.lua
+++ lua/debraj/keybinding.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]], {noremap = true})
-vim.keymap.set('n', '<Leader>y', '<esc>:%y+<CR>', { noremap = true })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]], { noremap = true })
+vim.keymap.set("n", "<Leader>y", "<esc>:%y+<CR>", { noremap = true })
+vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>ko", { noremap = true })
