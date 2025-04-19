6a755d50db1eb2379af1230208c6f82118c740b6
Debraj-Das <debrajdas.gayatri@gmail.com>
2025-04-20 01:47:59 +0530

    add the bracket autocompleted for first braket

--- lua/debraj/keybinding.lua
+++ lua/debraj/keybinding.lua
@@ -6,6 +6,7 @@ vim.keymap.set(
 )
 
 vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>ko", { noremap = true })
+vim.keymap.set("i", "(<CR>", "(<CR>)<Esc>ko", { noremap = true })
 vim.keymap.set("n", "<leader>v", "ggVG", { desc = "Whole [V]isual select", noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "[Y]ank to Clipboard", noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "[P]aste from Clipboard", noremap = true })
