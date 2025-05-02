66e4b389209a7998e7e1bb120cd0305402429374
Debraj-Das <debrajdas.gayatri@gmail.com>
2025-05-02 23:21:09 +0530

    change the color of the line number

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -4,9 +4,12 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		require('catppuccin').setup({
+		require("catppuccin").setup({
 			transparent_background = true,
 		})
 		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "green", bold = false })
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "green", bold = true })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "green", bold = false })
 	end,
 }
