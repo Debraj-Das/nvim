27246e2442a88055d15b5fc0dd161d1aecef368c
Debraj-Das <debrajdas.gayatri@gmail.com>
2026-03-01 02:35:31 +0530

    cpp template updated

--- snippets/cpp_template.lua
+++ snippets/cpp_template.lua
@@ -12,7 +12,6 @@ ls.add_snippets("cpp", {
 			"*/",
 			'#pragma GCC optimize("Ofast")',
 			'#pragma GCC optimize("unroll-loops")',
-			'#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")',
 			"",
 			"#include <bits/stdc++.h>",
 			"using namespace std;",
