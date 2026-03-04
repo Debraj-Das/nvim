2e6ed03bcd9fdcbef6687b3818654232760cea5a
Debraj-Das <debrajdas.gayatri@gmail.com>
2026-03-05 03:47:57 +0530

    run and complie keybinding update

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,1 +1,2 @@
-vim.keymap.set({"n", "v"}, "<F9>", ":! g++ -Wall -Wconversion -Wfatal-errors -g -std=c++17 -DLOCAL %<cr>", {buffer = true})
+vim.keymap.set({"n", "v"}, "<F9>", ":w <cr> :! g++ -Wall -Wconversion -Wfatal-errors -g -std=c++17 -DLOCAL %<cr>", {buffer = true})
+vim.keymap.set({"n", "v"}, "<F10>", ":!./a.out < 1.in", {buffer = true})
